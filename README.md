e13861becfde93b1a57f96a08b7c9baa3e81a16a
Bruno Cavalleri <cavalleri.bruno@gmail.com>
2024-01-09 17:22:04 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Requirements
+### For [folke/lazy.nvim](https://github.com/folke/lazy.nvim)
+- Download [JetBrains Mono Nerd Font](https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip)
+- Unzip in ```~/.local/share/fonts```
+- Run ```fc-cache -f -v```
+- Configure your terminal to use the installed font
+### For [nvim-telescope/telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
+- Install [BurntSushi/ripgrep](https://github.com/BurntSushi/ripgrep)
+- Install [sharkdp/fd](https://github.com/sharkdp/fd)
+### For [jedi-language-server](https://github.com/pappasam/jedi-language-server)
+- Install Python 3 venv module
+- ```sudo apt install python3-venv```
+# Instructions
+- Install neovim from source (see [instructions](https://github.com/neovim/neovim/blob/master/BUILD.md))
+- ```cd``` to ```~/.config/nvim```
+- ```git clone https://github.com/Cavalleri/nvim-config```
+- Run neovim
---
+++ init.lua
@@ -0,0 +1,223 @@
+-- Leader must be set before plugins are required
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Install folke/lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin list 
+require('lazy').setup({
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate'
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.5',
+		dependencies = {'nvim-lua/plenary.nvim'}
+	},
+	{
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			{
+				'williamboman/mason.nvim',
+				config = true
+			},
+			'williamboman/mason-lspconfig.nvim'
+		},
+	},
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+            {
+                'L3MON4D3/LuaSnip',
+                build = 'make install_jsregexp',
+                dependencies = {
+                    'rafamadriz/friendly-snippets',
+                }
+            },
+			'saadparwaiz1/cmp_luasnip',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-path',
+		}
+
+	},
+	{
+		'rose-pine/neovim',
+		name = 'rose-pine',
+		config = function()
+			vim.cmd.colorscheme('rose-pine')
+		end,
+	},
+	{
+		'numToStr/Comment.nvim',
+		lazy = false,
+	},
+	{
+		'windwp/nvim-autopairs',
+		event = 'InsertEnter',
+	},
+})
+
+-- LSP servers configs
+local servers = {
+    clangd = {},
+	lua_ls = {
+		Lua = {
+			workspace = {checkThirdParty = false},
+			telemetry = {enable = false},
+			diagnostics = {globals = {'vim'}},
+		}
+	},
+	jedi_language_server = {},
+	rust_analyzer = {},
+}
+
+-- Plugin configs
+local cmp = require('cmp')
+local configs = {
+	['nvim-treesitter.configs'] = {
+		ensure_installed = {'c', 'lua', 'python', 'rust', 'vim', 'vimdoc'},
+		highlight = {
+			enable = true,
+		},
+	},
+	['telescope'] = {},
+	['mason'] = {},
+	['mason-lspconfig'] = {
+		ensure_installed = vim.tbl_keys(servers),
+	},
+	['cmp'] = {
+		snippet = {
+			expand = function(args)
+				require('luasnip').lsp_expand(args.body)
+			end
+		},
+		mapping = cmp.mapping.preset.insert({
+			['<Tab>'] = cmp.mapping.select_next_item(),
+			['<S-Tab>'] = cmp.mapping.select_prev_item(),
+			['<CR>'] = cmp.mapping.confirm({
+				behavior = cmp.ConfirmBehavior.Replace,
+				select = true,
+			}),
+			['<C-Space>'] = cmp.mapping.complete(),
+			['<C-b>'] = cmp.mapping.scroll_docs(-4),
+			['<C-f>'] = cmp.mapping.scroll_docs(4),
+		}),
+		sources = cmp.config.sources({
+			{name = 'nvim_lsp'},
+			{name = 'luasnip'},
+			{name = 'path'},
+		}),
+	},
+	['Comment'] = {
+		toggler = {
+			line = '<Leader>cl',
+			block = '<Leader>cb',
+		},
+		opleader = {
+			line = '<Leader>cl',
+			block = '<Leader>cb',
+		},
+		extra = {
+			above = '<Leader>cO',
+			below = '<Leader>co',
+			eol = '<Leader>cA',
+		},
+	},
+	['nvim-autopairs'] = {},
+}
+for plugin, config in pairs(configs) do
+	require(plugin).setup(config)
+end
+require('luasnip.loaders.from_snipmate').lazy_load()
+require('luasnip').config.setup()
+cmp.event:on(
+	'confirm_done',
+	require('nvim-autopairs.completion.cmp').on_confirm_done()
+)
+
+-- neovim/nvim-lspconfig and williamboman/mason-lspconfig.nvim configs and LSP related keymaps
+local on_attach = function(_, buffer)
+	local keymaps = {
+		{'n', '<leader>rn', vim.lsp.buf.rename, {buffer = buffer, desc = 'LSP: rename symbol'}},
+		{'n', '<leader>gd', require('telescope.builtin').lsp_definitions, {buffer = buffer, desc = 'LSP: go to definition'}},
+	}
+	for _, keymap in pairs(keymaps) do
+		-- https://neovim.io/doc/user/lua.html
+		vim.keymap.set(unpack(keymap))
+	end
+end
+require('mason-lspconfig').setup_handlers({
+	function(server)
+		require('lspconfig')[server].setup({
+			capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+			on_attach = on_attach,
+			settings = servers[server],
+			filetypes = (servers[server] or {}).filetypes
+		})
+	end
+})
+
+-- Keymaps
+local keymaps = {
+	-- nvim-telescope/telescope.nvim keymaps
+	{'n', '<leader>ff', require('telescope.builtin').find_files, {desc = 'Find files'}},
+	{'n', '<leader>lg', require('telescope.builtin').live_grep, {desc = 'Live grep string'}},
+	{'n', '<leader>lb', require('telescope.builtin').buffers, {desc = 'List open buffers'}},
+	{'n', '<leader>fh', require('telescope.builtin').help_tags, {desc = 'Find help tag'}},
+	{'n', '<leader>fk', require('telescope.builtin').keymaps, {desc = 'Find keymaps'}},
+	-- Custom keymaps
+    {'v', 'J', ":m '>+1<CR>gv=gv", {desc = 'Move selected lines down'}},
+    {'v', 'K', ":m '<-2<CR>gv=gv", {desc = 'Move selected lines up'}},
+}
+for _, keymap in pairs(keymaps) do
+	-- https://neovim.io/doc/user/lua.html
+	vim.keymap.set(unpack(keymap))
+end
+
+-- Options
+local options = {
+    autoindent = true,
+	breakident = true,
+	completeopt = 'menuone,noselect',
+    cursorline = true,
+    -- TODO: modify highlight groups for this option to work with rose-pine
+    cursorlineopt = 'number,line',
+    equalalways = true,
+	expandtab = true,
+    hidden = false,
+	hlsearch = false,
+    incsearch = true,
+	ignorecase = true,
+    linebreak = true,
+	number = true,
+	relativenumber = true,
+    ruler = true,
+    scrolloff = 4,
+	signcolumn = 'yes',
+	shiftwidth = 4,
+	smartcase = true,
+	smartindent = true,
+	softtabstop = 4,
+    splitright = true,
+    splitbelow = true,
+	tabstop = 4,
+	termguicolors = true,
+	undofile = true,
+    wrap = false,
+}
+for option, value in pairs(options) do
+	vim.o[option] = value
+end
