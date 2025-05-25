7b8d18e6cb7b017c88afc50ff40f6885ecf7a936
Bruno Cavalleri <cavalleri.bruno@gmail.com>
2025-05-25 18:04:20 -0300

    Add treesitter, colorscheme and custom options

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Mini.nvim
 local path_package = vim.fn.stdpath('data') .. '/site'
 
 local mini_path = path_package .. '/pack/deps/start/mini.nvim'
@@ -23,14 +24,91 @@ local mini_plugins = {
 	operators = {},
 	pairs = {},
 	surround = {},
+
 	-- Appearance plugins
-	hues = {
-		background = '#002734',
-		foreground = '#c0c8cc',
-		saturation = 'high',
-	},
+	-- hues = {
+	--	background = '#002734',
+	--	foreground = '#c0c8cc',
+	--	saturation = 'high',
+	-- },
+
+	-- Package manager
+	deps = {},
 }
 
 for plugin, config in pairs(mini_plugins) do
 	require('mini.' .. plugin).setup(config)
+end
+
+
+-- Plugins
+local plugins = {
+	{
+		source = 'nvim-treesitter/nvim-treesitter',
+		-- hooks = { post_checkout =  function () vim.cmd('TSUpdate') end},
+		hooks = {build = ':TSUpdate'}
+	},
+	{
+		source = 'rose-pine/neovim',
+		hooks = {name = 'rose-pine'},
+	},
+}
+
+for _, plugin in ipairs(plugins) do
+		require('mini.deps').add(plugin)
+end
+
+-- Treesitter
+require('nvim-treesitter.configs').setup({
+		ensure_installed = {'c', 'python', 'lua', 'vim', 'vimdoc'},
+		highlight = {enable = true},
+})
+
+-- Rose Pine
+require('mini.deps').now(function() vim.cmd('colorscheme rose-pine') end)
+
+
+-- Keymaps
+local keymaps = {
+	{{"n", "v"}, "<Space>", "<Nop>", {desc = "Disables <Space> in normal and visual modes", silent = true}},
+	{"n", "<Esc>", ":nohlsearch<Enter>", {desc = "Disables search pattern highlight after a search"}},
+}
+
+for _, keymap in pairs(keymaps) do
+	-- https://neovim.io/doc/user/lua.html
+	vim.keymap.set(unpack(keymap))
+end
+
+
+-- Options
+local options = {
+	autoindent = true,
+	breakindent = true,
+	completeopt = "menuone,noselect",
+	cursorline = true,
+	expandtab = false, -- Use tabs instead of spaces
+	ignorecase = true,
+	incsearch = true,
+	laststatus = 3,
+	linebreak = true,
+	mouse = "",
+	number = true,
+	relativenumber = true,
+	scrolloff = 4,
+	shiftwidth = 4, -- Indentation level
+	smartcase = true,
+	smartindent = true,
+	softtabstop = -1, -- Editor behaviour when inserting or deleting tabs
+	splitbelow = true,
+	splitright = true,
+	statusline = "%<%F %h%m%r%=%(%l/%L, %c%)",
+	tabstop = 4, -- Tab width
+	termguicolors = true,
+	undofile = true,
+	wrap = false,
+	wrapscan = false,
+}
+
+for option, value in pairs(options) do
+	vim.o[option] = value
 end
